4b98f3530ceed079be71a9d13e8256a7e224aae7
Steve Masterman <steve@vermonster.com>
2012-03-26 13:30:14 -0400

    rails new copycat-demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CopycatDemo::Application.config.secret_token = '4d0d91ba1dd6036d66b125368bb6bbb0851b42666f07dea4842c698b28c6364cd6947f0dd32e3a17e1aa1b0ea9114136e44738806c944d68019b30293783bc3e'
