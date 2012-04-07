f8b013c91e8526e5158c9778bc0348064f6a34ae
Steve Masterman <steve@vermonster.com>
2012-04-07 16:00:25 -0400

    update copycat; remove extraneous gem requirements

--- config/initializers/copycat.rb
+++ config/initializers/copycat.rb
@@ -1,2 +1,5 @@
-COPYCAT_USERNAME = "copycat"
-COPYCAT_PASSWORD = "password"
+Copycat.setup do |config|
+  config.username = 'copycat'
+  config.password = 'password'
+  #config.route = 'copycat_translations'
+end
